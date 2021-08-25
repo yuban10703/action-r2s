e72d2a73d7fe073a35305e57feee32d0ded04b3b
yuban10703 <2846021566@qq.com>
2021-08-25 23:50:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/kongfl888/luci-app-kcpufreq ./package/lean/luci-app-kcpufreq
